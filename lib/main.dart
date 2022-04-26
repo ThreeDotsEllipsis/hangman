ebc6adaa5ca641f3d2f7bbe91760d4d9b86956d4
ThreeDots <threedotsellipsis@protonmail.com>
2022-04-26 20:05:26 +0500

    Wrong answers now work, time to figure out right answers

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,33 @@ class Hangman extends StatefulWidget {
 class _HangmanState extends State<Hangman> {
 
   final List<String> words = ["dancer", "mouse", "button", "jazz", "cat"];
+  final submitField = TextEditingController();
 
+  int mistakes = 0;
+  int maxMistakes = 6;
   String? secretWord;
   List<String> missedChars = [];
 
-  String pickRandomWord() {
-    words.shuffle();
-    secretWord = words[0];
-    return secretWord!;
+  void submitChar(char) {
+    if(false) {
+
+    }
+    else if(!missedChars.contains(char)) {
+      setState(() {
+        mistakes += 1;
+        missedChars.add(char);
+        submitField.clear();
+      });
+    }
+    else {
+      submitField.clear();
+    }
   }
 
-  void submitChar(char) {
-    setState(() {
-      missedChars.add(char);
-    });
+  @override
+  void initState() {
+    words.shuffle();
+    secretWord = words[0];
   }
 
   @override
@@ -41,37 +54,48 @@ class _HangmanState extends State<Hangman> {
       ),
       body: Column(
         children: [
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: pickRandomWord().split('').map((char) => Container(
-              padding: EdgeInsets.all(24), 
-              decoration: BoxDecoration(
-                border: Border.all()
-              ),
-            )).toList(),
-          ),
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: missedChars.map((char) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                char,
-                style: TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-            )).toList(),
-          ),
-          SizedBox(height: 24),
           TextField(
+            controller: submitField,
             onSubmitted: submitChar,
+            onEditingComplete: () {},
             textAlign: TextAlign.center,
             maxLength: 1,
             style: TextStyle(
               fontSize: 28,
             ),
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: secretWord!.split('').map((char) => Container(
+                  padding: EdgeInsets.all(16), 
+                  decoration: BoxDecoration(
+                    border: Border.all()
+                  ),
+                )).toList(),
+              ),
+              SizedBox(width: 16),
+              Text(
+                "${mistakes} / ${maxMistakes}",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Wrap(
+            children: missedChars.map((char) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                char,
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            )).toList(),
           ),
         ],
       ),
